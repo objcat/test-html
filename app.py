c117438fb7b49472f43362de7612de1bc2456378
不伤手的白猫 <13190164076@163.com>
2021-04-10 16:00:09 +0800

    完善k线图

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def hello_world():
     return redirect('/static/index.html')
 
 
-@app.route("/api/v1/ttjjHistoryList")
+@app.route("/api/v1/ttjj/ttjjHistoryList")
 @cross_origin(origins="*")
 def ttjjHistoryList():
     code = request.args.get("code")
@@ -38,6 +38,31 @@ def ttjjHistoryList():
     }
 
     res = requests.post("https://fundmobapi.eastmoney.com/FundMNewApi/FundMNHisNetList", headers=header, data=data)
+
+    return {"result": json.loads(res.text)}
+
+@app.route("/api/v1/ttjj/kList")
+@cross_origin(origins="*")
+def kList():
+    code = request.args.get("code")
+    pageSize = request.args.get("pageSize")
+
+    header = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    data = {
+        "secid": "0." + code,
+        "fields1": "f1,f2,f3,f4,f5,f6,f7,f8,f8,f10,f11",
+        "fields2": "f51,f52,f53,f54,f55,f56,f57",
+        "fqt": "0",
+        "lmt": "80",
+        "end": "20300101ut=128d100d314be19f356035cb850fe90c",
+        "klt": "101",
+        "authorityType": "fa"
+    }
+
+    res = requests.get("https://push2his.eastmoney.com/api/qt/stock/kline/get", headers=header, data=data)
 
     return {"result": json.loads(res.text)}
 
